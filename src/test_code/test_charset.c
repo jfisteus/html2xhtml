e85da28f75d7788da151dcf9f8376434a31e2c40
Jesus Arias Fisteus <jaf@it.uc3m.es>
2008-08-04 17:13:14 +0200

    The charset test program now receives the input charset as a parameter.

--- src/test_code/test_charset.c
+++ src/test_code/test_charset.c
@@ -3,19 +3,25 @@
 
 #include "../charset.h"
 
+/*
+ * call:
+ * test_charset <from_charset> <input_file>
+ *
+ */
+
 int main(int argc, char **argv)
 {
   char buf[128];
   int read;
 
-  if (argc == 2) {
-    FILE *file = fopen(argv[1], "r");
+  if (argc == 3) {
+    FILE *file = fopen(argv[2], "r");
     if (!file) {
       perror("fopen");
       exit(1);
     }
       
-    charset_init("ISO-8859-1", file);
+    charset_init(argv[1], file);
     do {
       read = charset_get_input(buf, sizeof(buf));
       fwrite(buf, 1, read, stdout);
